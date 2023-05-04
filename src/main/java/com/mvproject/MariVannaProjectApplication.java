68a861d564a2556b8f440185367af1b29e813a21
lichfb <lichfb@gmail.com>
2023-05-03 23:57:58 -0400

    UI created

---
+++ src/main/java/com/mvproject/MariVannaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mvproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariVannaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariVannaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvproject/MariVannaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariVannaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
